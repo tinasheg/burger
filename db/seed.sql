f72d574bf6499610ac141f878059494a85cf2910
tinashe <giyavha_tinashe@berkeley.edu>
2020-03-28 06:14:48 -0700

    changes text

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name)
-VALUES ('Big Mac'), ('Bison'), ('Triple Cheese');
+VALUES ('Big Mac'), ('Bison'), ('Triple Cheese'), ('Veggie'), ('Impossible'), ('Chicken');
